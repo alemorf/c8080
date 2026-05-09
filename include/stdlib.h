1dbd17cc8f30733f76b6ba3b353b87f87a73b76c
Alemorf <aleksey.f.morozov@gmail.com>
2026-05-10 02:38:14 +0300

    Add stdlib strtoi()

--- include/stdlib.h
+++ include/stdlib.h
@@ -36,6 +36,7 @@ typedef struct {
     long int quot, rem;
 } ldiv_t;
 
+int strtoi(const char *str, char **endptr, int base) __link("stdlib_h/strtoi.c");
 void srand(unsigned seed) __link("stdlib_h/rand.c");
 int rand(void) __link("stdlib_h/rand.c");
 int abs(int x) __link("stdlib_h/abs.c");
