2befa4e5ff1d0b1b97cb06ebc98da8289a9d6ccf
Alemorf <aleksey.f.morozov@gmail.com>
2026-05-13 13:55:55 +0300

    Stdlib hal ReadAndDecodeConsoleKeys()

---
+++ include/arch/vt100/c8080/console_h/readanddecodeconsolekeys.c
@@ -0,0 +1,43 @@
+/*
+ * c8080 stdlib
+ * Copyright (c) 2025 Aleksey Morozov aleksey.f.morozov@gmail.com aleksey.f.morozov@yandex.ru
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <c8080/console.h>
+#include <stdio.h>
+
+uint16_t ReadAndDecodeConsoleKeys(void) {
+    uint8_t c = getchar();
+    if (c != 0x1B)
+        return c;
+    c = getchar();
+    if (c != '[') {
+        if (c == 0x1B)
+          return c;
+        return 0x100 | c;
+    }
+    c = getchar();
+    return 0x200 + c;
+
+    /* TODO: Other multi-byte key codes:
+     * [OP = F1
+     * [OQ = F2
+     * [OR = F3
+     * [2~ = INS
+     * [3~ = DEL
+     * [5~ = PAGE UP
+     * [6~ = PAGE DOWN
+     */
+}
