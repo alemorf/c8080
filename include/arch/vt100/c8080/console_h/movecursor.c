c399bfcbc35c042123bb3d52f8ce1a4c23fd86be
Alemorf <aleksey.f.morozov@gmail.com>
2026-05-13 17:27:07 +0300

    Stdlib. VT100. MoveCursor, ShowCursor, HideCursor.

---
+++ include/arch/vt100/c8080/console_h/movecursor.c
@@ -0,0 +1,23 @@
+/*
+ * c8080 stdlib
+ * Copyright (c) 2025 Aleksey Morozov aleksey.f.morozov@gmail.com aleksey.f.morozov@yandex.ru
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <c8080/console.h>
+#include <stdio.h>
+
+void MoveCursor(uint8_t x, uint8_t y) {
+    printf("\x1B[%u;%uH", x, y);
+}
