69dc92fdf8ebefad3efa2020560c2a8a3152c9ae
Alemorf <aleksey.f.morozov@gmail.com>
2026-05-18 11:43:53 +0300

    Compiler. Code simplification. NumberIsNonZero().

--- c8080_source/c/tools/numberisnonzero.cpp
+++ c8080_source/c/tools/numberisnonzero.cpp
@@ -15,33 +15,14 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "numberisnonzero.h"
 #include "numberiszero.h"
-#include "cthrow.h"
 
 bool NumberIsNonZero(CConstNodePtr node) {
     assert(node != nullptr);
 
-    // TODO: CNT_CONVERT can truncate non zero to zero
-
-    if (node->type == CNT_NUMBER) {
-        if (node->ctype.IsPointer())
-            return node->number.u == 0;
-        switch (node->ctype.base_type) {
-            case CBT_CHAR:
-            case CBT_SIGNED_CHAR:
-            case CBT_SHORT:
-            case CBT_INT:
-            case CBT_LONG:
-            case CBT_LONG_LONG:
-                return node->number.i != 0;
-            case CBT_UNSIGNED_CHAR:
-            case CBT_UNSIGNED_SHORT:
-            case CBT_UNSIGNED_INT:
-            case CBT_UNSIGNED_LONG:
-            case CBT_UNSIGNED_LONG_LONG:
-                return node->number.u != 0;
-        }
-    }
+    if (node->type == CNT_NUMBER)
+        return !NumberIsZero(node);
 
     return false;
 }
