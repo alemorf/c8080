10b56a1bf43f3755064515ab09cc220add0dbd6e
Alemorf <aleksey.f.morozov@gmail.com>
2026-05-19 11:04:24 +0300

    Compiler. Comments for cvariablemode.h

--- c8080_source/c/cvariablemode.h
+++ c8080_source/c/cvariablemode.h
@@ -1,6 +1,6 @@
 /*
  * c8080 compiler
- * Copyright (c) 2025 Aleksey Morozov aleksey.f.morozov@gmail.com aleksey.f.morozov@yandex.ru
+ * Copyright (c) 2026 Aleksey Morozov aleksey.f.morozov@gmail.com aleksey.f.morozov@yandex.ru
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,4 +17,8 @@
 
 #pragma once
 
-enum CVariableMode { CVM_NOT_SET, CVM_STACK, CVM_GLOBAL };
+enum CVariableMode {
+    CVM_NOT_SET,
+    CVM_STACK,  // attribute __static
+    CVM_GLOBAL  // attribute __global
+};
