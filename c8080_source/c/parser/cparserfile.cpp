c1c443fb36a317db51488f10691778d4d7315fdd
Alemorf <aleksey.f.morozov@gmail.com>
2026-07-19 02:06:20 +0300

    Compiler. Fix error "abc\0def".

--- c8080_source/c/parser/cparserfile.cpp
+++ c8080_source/c/parser/cparserfile.cpp
@@ -892,7 +892,7 @@ CNodePtr CParserFile::ParseExpressionValue() {
         result->ctype.base_type = CBT_CHAR;
         result->ctype.flag_const = true;
         result->ctype.pointers.push_back(CPointer(str.size() + 1));
-        result->const_string = programm.RegisterConstString(programm.SaveString(str));
+        result->const_string = programm.RegisterConstString(CString(programm.SaveString(str), str.size()));
         return result;
     }
 
